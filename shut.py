eaa70f551fca7f6af8a640a98e8d04d47312e5c8
Abhishek <idiot63666@gmail.com>
2022-12-30 20:12:57 +0530

    Update shut.py

--- shut.py
+++ shut.py
@@ -2,19 +2,70 @@ from tkinter import  *
 import os   
 
 root = Tk()
-root.title()
-root.geomtry("400x580")
+root.title("Shutdown App")
+root.geometry("400x580")
+
+def restarttime():
+    os.system("shutdown /r /t 30")
+
+def restart():
+    os.system("shutdown /r /t 1")
+
+def shutdown():
+    os.system("shutdown /s /t 1")
+
+def logout():
+    os.system("shutdown -1")
 
 #First button 
 restart_time_button = PhotoImage(file="restart time.png")
 
-first_button =Button(root,image=restart_time_button,borderwidth=0,cursor="hand2")
+first_button =Button(root,image=restart_time_button,borderwidth=0,cursor="hand2",command=restarttime)
 first_button.place(x=10,y=10)
 
 #Second button
 close_button = PhotoImage(file="close.png")
 
-second_button = Button(root,image=close_button,borderwidth=0,cursor="hand2")
+second_button = Button(root,image=close_button,borderwidth=0,cursor="hand2",command=root.destroy)
 second_button.place(x=200,y=10)
+
+#Third button
+restart_button = PhotoImage(file="restart.png")
+
+third_button = Button(root,image=restart_button,borderwidth=0,cursor="hand2",command=restart)
+third_button.place(x=10,y=200)
+
+#Fourth button
+shutdown_button = PhotoImage(file="shutdown.png")
+
+fourth_button = Button(root,image=shutdown_button,borderwidth=0,cursor="hand2",command=shutdown)
+fourth_button.place(x=200,y=200)
+
+#Fifth button
+logout_button = PhotoImage(file="logout.png")
+
+fifth_button = Button(root,image=logout_button,borderwidth=0,cursor="hand2",command=logout)
+fifth_button.place(x=10,y=400)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 root.mainloop()
